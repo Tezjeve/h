c91f23443c06924ea5004ab5e6f0311668bdf24c
Tezjeve <130067328+Tezjeve@users.noreply.github.com>
2025-02-20 17:25:27 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
-})
+namespace smartData {
+
+
+//% block="send data %data to register %register"
+export function sendData(data: string, register: number){
+    let save: string
+    save = data
+    serial.writeLine("saved your data : (" + data + ") to register : (" + (register) + ")")
+    serial.writeLine("the next line is for the microbit to understand")
+    serial.writeLine("")
+    }
+}
