d20ca09ccbf6f92e7f6abed85b41e8dcab2fcc8e
Tezjeve <130067328+Tezjeve@users.noreply.github.com>
2025-02-21 12:16:49 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,19 @@
 namespace smartData {
 
+//% block="retrieve data %registerReciever"
+//% group="data handeling"
+    export function retrieveData(registerReciever: number) {
+        let registernumber: number
+        let line: string
+        registernumber = registerReciever
+        line = serial.readLine(registernumber)
+    }
 
-//% block="send data %data to register %register"
-export function sendData(data: string, register: number){
-    let save: string
-    save = data
-    serial.writeLine("saved your data : (" + data + ") to register : (" + (register) + ")")
-    serial.writeLine("the next line is for the microbit to understand")
-    serial.writeLine("")
+    //% block="send data %data to register %register"
+    //% group="data handeling"
+    export function sendData(data: string, register: number) {
+        let save: string
+        save = data
+        serial.writeLine("sDd:= " + data + " sdR:=+ " + register + " SmDNonu")
     }
 }
